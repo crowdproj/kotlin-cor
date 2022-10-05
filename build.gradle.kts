7456924b538f2346f96f2b7870d80f6adb64343c
Sergey Okatov <sokatov@gmail.com>
2022-10-05 09:22:02 +0500

    #8 Fix for disordered workers execution in subChain, fix ci/cd pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -195,6 +195,7 @@ tasks {
         reports {
             junitXml.required.set(true)
         }
+        setupTestLogging()
     }
 
     publish {
@@ -207,4 +208,39 @@ tasks {
 //        dependsOn(closeAndReleaseRepository)
     }
 
+}
+
+fun Test.setupTestLogging() {
+    testLogging {
+        events(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+        )
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+
+        addTestListener(object : TestListener {
+            override fun beforeSuite(suite: TestDescriptor) {}
+            override fun beforeTest(testDescriptor: TestDescriptor) {}
+            override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+            override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                if (suite.parent != null) { // will match the outermost suite
+                    val output = "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} passed, ${result.failedTestCount} failed, ${result.skippedTestCount} skipped)"
+                    val startItem = "|  "
+                    val endItem = "  |"
+                    val repeatLength = startItem.length + output.length + endItem.length
+                    val messages = """
+                        ${(1..repeatLength).joinToString("") { "-" }}
+                        $startItem$output$endItem
+                        ${(1..repeatLength).joinToString("") { "-" }}
+                    """.trimIndent()
+                    println(messages)
+                }
+            }
+        })
+    }
 }
